6703e47dade592ede9e68e16e1cada9c6396f9c5
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-08-26 10:03:18 +0530

    student backend completed

--- src/main/java/com/icet/paymentapp/repo/StudentRepo.java
+++ src/main/java/com/icet/paymentapp/repo/StudentRepo.java
@@ -8,14 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 @EnableJpaRepositories
 public interface StudentRepo extends JpaRepository<Student,String> {
     Student findTopByStudentIdStartsWithOrderByStudentIdDesc(String prefix);
+
     @Query(value = "SELECT * FROM student WHERE student_id LIKE %?1% OR full_name LIKE %?1%",nativeQuery = true)
     Page<Student> searchStudentsByText(String text,PageRequest pageRequest);
+
     @Query(value = "SELECT COUNT(student_id) FROM student WHERE student_id LIKE %?1% OR full_name LIKE %?1%",nativeQuery = true)
     long searchCountOfStudentsByText(String text);
 }
